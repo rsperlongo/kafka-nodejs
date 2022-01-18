3ead3b07b3a1e71d0b1b1eeabc4a0019429b5efc
Ricardo Sperlongo DECSKILL <RicardoSperlongo.DECSKILL@edp.com>
2022-01-18 14:36:55 -0300

    kafka wiht nodejs

---
+++ index.js
@@ -0,0 +1,10 @@
+const produce = require('./producer');
+const consume = require('./consumer');
+
+produce().catch((err) => {
+    console.error('error in producer', err)
+})
+
+consume().catch((err) => {
+    console.error('error in consumer', err)
+})
